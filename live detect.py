4768e99c9beaad14626154dec93ee6c8befe8ef5
Syskey SK <139443204+PRATIKK0709@users.noreply.github.com>
2023-11-03 09:38:17 +0530

    Update live detect.py

--- live detect.py
+++ live detect.py
@@ -2,80 +2,83 @@ import cv2
 import numpy as np
 import pytesseract
 import datetime
-import imutils
 
-pytesseract.pytesseract.tesseract_cmd = r'tesseract path'
+# Set the path to the Tesseract executable
+pytesseract.pytesseract.tesseract_cmd = r'/opt/homebrew/bin/tesseract'
 
-# Initialize the video capture from the default camera (usually camera index 0).
-cap = cv2.VideoCapture(0)  # Use 0 for the default camera, or specify a video file path.
-
-# Initialize a flag to control the loop
-exit_key = ord('q')
-
-while True:
-    ret, frame = cap.read()
-
-    if not ret:
-        print("Error: Unable to capture video.")
-        break
-
-    # Preprocess the frame as you did in your initial code.
-    img = cv2.resize(frame, (600, 400))
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+def preprocess_image(frame):
+    # Preprocess the frame
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     gray = cv2.bilateralFilter(gray, 13, 15, 15)
-    
-    # Apply thresholding to segment the license plate characters from the background.
-    _, thresh = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
-    
-    # Find contours in the thresholded image.
-    contours, _ = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    return gray
+
+def find_license_plate(gray):
+    # Detect license plate contours
+    edged = cv2.Canny(gray, 30, 200)
+    contours, _ = cv2.findContours(edged.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
     contours = sorted(contours, key=cv2.contourArea, reverse=True)[:10]
     screenCnt = None
 
     for c in contours:
         peri = cv2.arcLength(c, True)
-        approx = cv2.approxPolyDP(c, 0.02 * peri, True)
+        approx = cv2.approxPolyDP(c, 0.018 * peri, True)
 
         if len(approx) == 4:
             screenCnt = approx
             break
 
-    if screenCnt is not None:
-        # Draw license plate and display results.
-        cv2.drawContours(img, [screenCnt], -1, (0, 0, 255), 3)
-        
-        mask = np.zeros(gray.shape, np.uint8)
-        new_image = cv2.drawContours(mask, [screenCnt], 0, 255, -1)
-        new_image = cv2.bitwise_and(img, img, mask=mask)
+    return screenCnt
 
-        (x, y) = np.where(mask == 255)
-        (topx, topy) = (np.min(x), np.min(y))
-        (bottomx, bottomy) = (np.max(x), np.max(y))
-        Cropped = gray[topx:bottomx + 1, topy:bottomy + 1]
+def recognize_license_plate(gray, screenCnt):
+    if screenCnt is None:
+        return "No contour detected", None
 
-        text = pytesseract.image_to_string(Cropped, config='--psm 8')
+    # Draw license plate
+    mask = np.zeros(gray.shape, np.uint8)
+    new_image = cv2.drawContours(mask, [screenCnt], 0, 255, -1)
+    new_image = cv2.bitwise_and(gray, gray, mask=mask)
 
-        if text.strip():  # Check if the recognized text is not empty
-            # Get the current date and time
-            current_datetime = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    (x, y) = np.where(mask == 255)
+    (topx, topy) = (np.min(x), np.min(y))
+    (bottomx, bottomy) = (np.max(x), np.max(y))
+    cropped = gray[topx:bottomx + 1, topy:bottomy + 1]
 
-            # Create the text to be saved
-            output_text = f"{text.strip()}"
-            datetimesave = f"{current_datetime}"
+    # Extract text from the license plate
+    text = pytesseract.image_to_string(cropped, config='--psm 11')
+    return text, cropped
 
-            # Save the detected license plate number and date/time in the specified format in a text file
-            with open("detected_license_plate.txt", "w") as text_file:
-                text_file.write(f"{output_text} | {datetimesave}")
+def save_result(text):
+    # Get the current date and time
+    current_datetime = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
-            print("Programming Fever's License Plate Recognition")
-            print(f"Detected license plate Number is: {text}")
-        
-    # Display the original frame.
-    cv2.imshow('Car', img)
+    # Create the text to be saved
+    output_text = f"{text.strip()} | {current_datetime}"
 
-    # Check for the exit key to quit the loop.
-    if cv2.waitKey(1) & 0xFF == exit_key:
-        break
+    # Save the detected license plate number and date/time in a text file
+    with open("detected_license_plate.txt", "w") as text_file:
+        text_file.write(output_text)
 
-cap.release()
-cv2.destroyAllWindows()
+def main():
+    # Initialize the webcam capture
+    cap = cv2.VideoCapture(0)  # Use the default camera (usually index 0)
+
+    while True:
+        ret, frame = cap.read()  # Read a frame from the webcam
+        gray = preprocess_image(frame)
+        screenCnt = find_license_plate(gray)
+        text, cropped = recognize_license_plate(gray, screenCnt)
+
+        # Display the result in the frame
+        if screenCnt is not None:
+            cv2.putText(frame, f"License Plate: {text.strip()}", (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+            cv2.imshow('Frame', frame)
+
+        if cv2.waitKey(1) & 0xFF == 27:  # Press 'Esc' to exit the loop
+            break
+
+    # Release the webcam and close all OpenCV windows
+    cap.release()
+    cv2.destroyAllWindows()
+
+if __name__ == "__main__":
+    main()
